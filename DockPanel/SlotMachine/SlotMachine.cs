af3f8e25ed82635c2c7a53b81a03bd4d82a47ed3
lena <islenaliny@gmail.com>
2020-09-01 11:22:14 +0800

    Added SlotMachine project

---
+++ DockPanel/SlotMachine/SlotMachine.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using WeifenLuo.WinFormsUI.Docking;
+using System.Resources;
+using System.Reflection;
+
+namespace Practice.SlotMachine
+{
+    /// 拉Bar遊戲
+    /// 資料來源: https://dotblogs.com.tw/joe_ziqiao_wang/2017/09/12/135022
+    public partial class SlotMachine : DockContent
+    {
+        int a, b, c = 0;
+
+        public SlotMachine()
+        {
+            InitializeComponent();
+            this.timer1.Enabled = false;
+
+            this.timer1.Tick += new EventHandler(this.TimerTick);
+            this.btnRotate.Click += new EventHandler(this.ButtonClick);
+            this.btnExit.Click += new EventHandler(this.ButtonClick);
+        }
+
+        private void TimerTick(object sender, EventArgs e)
+        {
+
+            Random random = new Random();
+            a = random.Next(1, 4);
+            b = random.Next(1, 4);
+            c = random.Next(1, 4);
+
+            switch (a)
+            {
+                case 1:
+                    this.pictureBox1.Image = Practice.SlotMachine.Resource_SlotMachine.one;
+                    break;
+                case 2:
+                    this.pictureBox1.Image = Practice.SlotMachine.Resource_SlotMachine.Snoopy;
+                    break;
+                case 3:
+                    this.pictureBox1.Image = Practice.SlotMachine.Resource_SlotMachine.JailRabbit;
+                    break;
+            }
+            switch (b)
+            {
+                case 1:
+                    this.pictureBox2.Image = Practice.SlotMachine.Resource_SlotMachine.one;
+                    break;
+                case 2:
+                    this.pictureBox2.Image = Practice.SlotMachine.Resource_SlotMachine.Snoopy;
+                    break;
+                case 3:
+                    this.pictureBox2.Image = Practice.SlotMachine.Resource_SlotMachine.JailRabbit;
+                    break;
+            }
+            switch (c)
+            {
+                case 1:
+                    this.pictureBox3.Image = Practice.SlotMachine.Resource_SlotMachine.one;
+                    break;
+                case 2:
+                    this.pictureBox3.Image = Practice.SlotMachine.Resource_SlotMachine.Snoopy;
+                    break;
+                case 3:
+                    this.pictureBox3.Image = Practice.SlotMachine.Resource_SlotMachine.JailRabbit;
+                    break;
+            }
+        }
+
+        private void ButtonClick(object sender, EventArgs e)
+        {
+            if (sender.Equals(this.btnRotate))
+            {
+                if (this.btnRotate.Text == "Rotate")
+                {
+                    this.timer1.Enabled = true;
+                    this.btnRotate.Text = "Stop";
+                }
+                else
+                {
+                    this.timer1.Enabled = false;
+                    this.btnRotate.Text = "Rotate";
+
+                    if (a == b && b == c && a == c)
+                        MessageBox.Show("You win!", this.Text);
+                }
+            }
+            else if (sender.Equals(this.btnExit))
+            {
+                this.Close();
+            }
+        }
+    }
+}
---
+++ DockPanel/SlotMachine/SlotMachine.designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Practice.SlotMachine
+{
+    partial class SlotMachine
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnRotate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnRotate
+            // 
+            this.btnRotate.Location = new System.Drawing.Point(422, 51);
+            this.btnRotate.Name = "btnRotate";
+            this.btnRotate.Size = new System.Drawing.Size(75, 44);
+            this.btnRotate.TabIndex = 3;
+            this.btnRotate.Text = "Rotate";
+            this.btnRotate.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(422, 104);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 44);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(287, 42);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 116);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(162, 42);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 116);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(40, 42);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 116);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // SlotMachine
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(528, 196);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnRotate);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "SlotMachine";
+            this.Text = "SlotMachine";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button btnRotate;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+

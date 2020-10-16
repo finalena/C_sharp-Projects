d8102aa733c1cb39a20a481ea722ed483f8a4f13
lena <islenaliny@gmail.com>
2020-10-16 14:53:50 +0800

    Added Practice1 project

---
+++ DockPanel/Practice1/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using WeifenLuo.WinFormsUI.Docking;
+
+namespace Practice.Practice1
+{
+    /// 計算列印頁數
+    /// 來源: http://www.webapp.com.tw/EBook5/view.aspx?a=1&TreeNodeID=73&id=988
+    public partial class Form1 : DockContent
+    {
+        List<Section> SectionList = new List<Section>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+            this.btnSubmit.Click += new EventHandler(Button_click);
+            this.btnClear.Click += new EventHandler(Button_click);
+        }
+
+        private void Button_click(object sender, EventArgs e)
+        {
+            if (sender.Equals(this.btnSubmit))
+            {
+                int iStart = 0;
+                int iEnd = 0;
+                if (!int.TryParse(this.txtStart.Text, out iStart) || !int.TryParse(this.txtEnd.Text, out iEnd))
+                {
+                    MessageBox.Show("請輸入數值", this.Text);
+                    return; 
+                }
+
+                if (iStart <= 0 || iEnd <= 0)
+                {
+                    MessageBox.Show("頁數不可小於0", this.Text);
+                    return;
+                }
+
+                Section obj = new Section(iStart, iEnd);
+                this.txtResult.Text = obj.ToString();
+
+                for (int i = SectionList.Count-1 ; i >= 0 ; i--)
+                {
+                    Section check = SectionList[i];
+                    if (check.MustRemove(ref obj))
+                    {
+                        SectionList.Remove(check);
+                    }
+                }
+
+                SectionList.Add(new Section(obj.Start, obj.End));
+                ShowResult();
+            }
+            else if (sender.Equals(this.btnClear))
+            {
+                SectionList.Clear();
+                this.txtStart.Text = "";
+                this.txtEnd.Text = "";
+                this.txtResult.Text = "";
+            }
+        }
+
+        private void ShowResult() 
+        {
+            SectionList.Sort();
+
+            StringBuilder sb = new StringBuilder();
+            foreach (Section item in SectionList)
+            {
+                sb.Append(item.ToString() + ",");
+            }
+            MessageBox.Show(sb.ToString(), this.Text);
+        }
+    }
+
+    class Section : IComparable
+    {
+        private int iStart;
+        private int iEnd;       
+
+        public int Start
+        {
+            get { return iStart; }
+            set 
+            {
+                if (value % 2 == 0)
+                    iStart = value - 1;
+                else
+                    iStart = value; 
+            }
+        }
+
+        public int End 
+        {
+            get { return iEnd; }
+            set 
+            {
+                if (value % 2 == 0)
+                    iEnd = value;
+                else
+                    iEnd = value + 1;
+            }
+        }
+            
+        public Section(int iStart, int iEnd) 
+        {
+            int iStartValue = Math.Min(iStart, iEnd);
+            int iEndValue = Math.Max(iStart, iEnd);
+
+            this.Start = iStartValue;
+            this.End = iEndValue;
+        }
+
+        public bool MustRemove(ref Section obj)
+        {
+            if (obj.End + 1 < this.Start || obj.Start > this.End + 1)
+            {
+                return false;
+            }
+            else
+            {//二者有重疊, 變動target值後傳回true
+                obj.Start = Math.Min(obj.Start, this.Start);
+                obj.End = Math.Max(obj.End, this.End);
+
+                return true;
+            }
+        }
+
+        public int CompareTo(object obj) 
+        {
+            return this.Start.CompareTo(((Section)obj).Start);
+        }
+        
+        public override string ToString()
+        {
+            return Start + "-" + End;
+        }
+    }
+}
---
+++ DockPanel/Practice1/Form1.designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Practice.Practice1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.txtStart = new System.Windows.Forms.TextBox();
+            this.txtEnd = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "起始頁";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "結束頁";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "單筆範圍";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(106, 167);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(99, 23);
+            this.btnSubmit.TabIndex = 3;
+            this.btnSubmit.Text = "新增此列印範圍";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(24, 167);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "重設";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // txtStart
+            // 
+            this.txtStart.Location = new System.Drawing.Point(94, 32);
+            this.txtStart.Name = "txtStart";
+            this.txtStart.Size = new System.Drawing.Size(100, 22);
+            this.txtStart.TabIndex = 5;
+            // 
+            // txtEnd
+            // 
+            this.txtEnd.Location = new System.Drawing.Point(94, 74);
+            this.txtEnd.Name = "txtEnd";
+            this.txtEnd.Size = new System.Drawing.Size(100, 22);
+            this.txtEnd.TabIndex = 6;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(94, 118);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(100, 22);
+            this.txtResult.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(229, 228);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtEnd);
+            this.Controls.Add(this.txtStart);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "計算列印頁數";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox txtStart;
+        private System.Windows.Forms.TextBox txtEnd;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
+

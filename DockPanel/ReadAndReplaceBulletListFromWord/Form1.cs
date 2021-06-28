86cb492c673653ef34af403db503554cc5dad25a
lena <islenaliny@gmail.com>
2021-06-28 16:49:38 +0800

    修改程式

--- DockPanel/ReadAndReplaceBulletListFromWord/Form1.cs
+++ DockPanel/ReadAndReplaceBulletListFromWord/Form1.cs
@@ -27,42 +27,42 @@ namespace Practice.ReadAndReplaceBulletListFromWord
             Word.Application MyWord = new Word.Application();
             Word.Document MyDoc = MyWord.Documents.Open(DefaultPath + @"\ReadAndReplaceBulletListFromWord\test.docx");
             MyWord.Visible = true;
-
-            //替換□符號
-            object strStart = "□";
-            Word.Range MyRange = MyDoc.Content;
             Word.Range rngTmp = MyDoc.Range(0, 0);
-            MyRange.Find.Text = "□";
-            MyRange.Find.Forward = true;
-            MyRange.Find.Execute(strStart);
-            while (MyRange.Find.Found)
-            {
-                //游標處核取方塊控制
-                rngTmp.SetRange(MyRange.Start, MyRange.End);
-                rngTmp.Select();
-                Word.ContentControl checkbox = rngTmp.ContentControls.Add(Word.WdContentControlType.wdContentControlCheckBox);
-                checkbox.SetCheckedSymbol(82, "Wingdings 2");
-       
-                //游標移至被加入的核取方塊後
-                MyRange.Collapse(Word.WdCollapseDirection.wdCollapseEnd);
-                MyRange.Select();
 
-                MyRange.Find.Execute(strStart);
-            }
-
-            //替換項目符號
+            //將項目符號取代成指定符號
             foreach (Word.Paragraph p in MyWord.ActiveDocument.Paragraphs)
             {
                 if (p.Range.ListFormat.ListType != Word.WdListType.wdListBullet) continue;
                 
                 //移除項目符號
                 p.Range.ListFormat.ApplyBulletDefault(DefaultListBehavior: Word.WdDefaultListBehavior.wdWord9ListBehavior);
-                //加入核取方塊控制項
+               
                 p.Range.Select();
                 MyWord.Selection.HomeKey(Word.WdUnits.wdLine, Word.WdMovementType.wdMove);
                 rngTmp = MyWord.Selection.Range;
-                Word.ContentControl checkbox2 = rngTmp.ContentControls.Add(Word.WdContentControlType.wdContentControlCheckBox);
-                checkbox2.SetCheckedSymbol(82, "Wingdings 2");
+                rngTmp.Text = "□";
+            }
+
+            //替換指定符號為核取方塊控制
+            object strStart = "□";
+            Word.Range MyRange = MyDoc.Content;
+            MyRange.Find.Text = "□";
+            MyRange.Find.Forward = true;
+            MyRange.Find.Execute(strStart);
+
+            while (MyRange.Find.Found)
+            {
+                //選取游標取代
+                rngTmp.SetRange(MyRange.Start, MyRange.End);
+                rngTmp.Select();
+                Word.ContentControl checkbox = rngTmp.ContentControls.Add(Word.WdContentControlType.wdContentControlCheckBox);
+                checkbox.SetCheckedSymbol(82, "Wingdings 2");
+
+                //游標移至被加入的核取方塊後
+                MyRange.Collapse(Word.WdCollapseDirection.wdCollapseEnd);
+                MyRange.Select();
+                MyRange.Font.Size = 14;
+                MyRange.Find.Execute(strStart);
             }
 
             MessageBox.Show("處理完成");
